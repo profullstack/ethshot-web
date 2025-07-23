516db8ac3f6c02b2778046fe4f1bb5f9b049321e
Anthony Ettinger <anthony@chovy.com>
2025-07-23 02:21:38 -0700

    chore: remove unused SvelteKit config options and simplify svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -13,28 +13,6 @@ const config = {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
     
-    // App configuration
-    appDir: 'app',
-    
-    // Files configuration
-    files: {
-      assets: 'public',
-      hooks: {
-        client: 'src/hooks.client.js',
-        server: 'src/hooks.server.js'
-      },
-      lib: 'src/lib',
-      params: 'src/params',
-      routes: 'src/routes',
-      serviceWorker: 'src/service-worker.js',
-      template: 'src/app.html'
-    },
-    
-    // Environment variables
-    env: {
-      publicPrefix: 'PUBLIC_'
-    },
-    
     // CSP configuration for Web3 compatibility
     csp: {
       mode: 'auto',
@@ -43,14 +21,6 @@ const config = {
         'object-src': ['none'],
         'base-uri': ['self']
       }
-    },
-    
-    // Preload strategy
-    preloadStrategy: 'modulepreload',
-    
-    // Version configuration
-    version: {
-      name: process.env.npm_package_version
     }
   }
 };
