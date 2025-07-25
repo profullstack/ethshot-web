c9bdd3ab4b4fed54bfb99cf5ac9191e2d98e7e74
Anthony Ettinger <anthony@chovy.com>
2025-07-25 16:52:10 -0700

    feat: update toggle button styles and change manifest category from finance to crypto

--- src/lib/components/UserProfile.svelte
+++ src/lib/components/UserProfile.svelte
@@ -371,7 +371,7 @@
                 class="sr-only peer"
                 disabled={saving || $uploadingAvatar}
               />
-              <div class="w-11 h-6 bg-gray-600 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+              <div class="w-11 h-6 bg-gray-600 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
             </label>
           </div>
         </div>
